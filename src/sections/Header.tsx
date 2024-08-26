04bed22832b376599c78b6acdc80f23295f6b964
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-08-26 11:45:22 +0300

    Completed all homepage components

---
+++ src/sections/Header.tsx
@@ -0,0 +1,62 @@
+import LogoIcon from "@/assets/logo.svg";
+import MenuIcon from "@/assets/icon-menu.svg";
+import Button from "@/components/Button";
+
+export const Header = () => {
+  return (
+    <header className="sticky py-4 border-b border-white/15 md:border-none top-0 z-10">
+      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
+      
+      <div className="container">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto relative">
+          <div className="absolute inset-0 backdrop-blur -z-10  hidden md:block"></div>
+
+          <div>
+            <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
+              <LogoIcon className="h-8 w-8"/>
+            </div>
+          </div>
+
+          <div className="hidden md:block">
+            <nav className="flex gap-8 text-sm">
+              {NAVIGATION.map((item) => (
+                <a 
+                  href={item.href} 
+                  key={item.label}
+                  className="text-white/70 hover:text-white transition"
+                >
+                  {item.label}
+                </a>
+              ))}
+            </nav>
+          </div>
+
+          <div className="flex gap-4 items-center">
+            <Button> Join waitlist </Button>
+            <MenuIcon className="md:hidden"/>
+          </div>
+
+        </div>
+      </div>
+    </header>
+  );
+};
+
+const NAVIGATION = [
+  {
+    label: "Features",
+    href: "#",
+  },
+  {
+    label: "Developers",
+    href: "#",
+  },
+  {
+    label: "Pricing",
+    href: "#",
+  },
+  {
+    label: "Changelog",
+    href: "#",
+  },
+]
